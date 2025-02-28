7a97156ba4bc4e35a17870ff2e100cafcc727f71
Alonso Decarli <alonsodecarli@gmail.com>
2025-02-28 15:21:32 -0300

    Teste 1

--- src/test/java/br/com/viasoft/app1/App1ApplicationTests.java
+++ src/test/java/br/com/viasoft/app1/App1ApplicationTests.java
@@ -3,11 +3,13 @@ package br.com.viasoft.app1;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.util.Assert.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.Assert;
 
 
 @WebMvcTest(App1Application.class)
@@ -15,6 +17,15 @@ class App1ApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@Test
+	void contextLoads() {
+		// Teste que verifica se a aplicação inicia sem falhas
+		App1Application.main(new String[]{});
+
+		//Add at least one assertion to this test case.
+		Assert.isTrue(true, "Teste de contexto");
+	}
 
 	@Test
 	void testHelloWithoutName() throws Exception {

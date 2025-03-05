b960f8f03c907b6f892da401ce86bdfb88ddad9d
Alonso Decarli <alonsodecarli@gmail.com>
2025-03-05 09:45:29 -0300

    Teste 1

--- src/main/java/br/com/viasoft/app1/App1Application.java
+++ src/main/java/br/com/viasoft/app1/App1Application.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class App1Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(App1Application.class, args);
 	}
--- src/test/java/br/com/viasoft/app1/App1ApplicationTests.java
+++ src/test/java/br/com/viasoft/app1/App1ApplicationTests.java
@@ -1,10 +1,5 @@
 package br.com.viasoft.app1;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.util.Assert.*;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -27,17 +22,6 @@ class App1ApplicationTests {
 		Assert.isTrue(true, "Teste de contexto");
 	}
 
-	@Test
-	void testHelloWithoutName() throws Exception {
-		mockMvc.perform(get("/hello")) // Faz GET em /hello
-				.andExpect(status().isOk()) // Espera HTTP 200
-				.andExpect(content().string("Hello World!")); // Espera "Hello World!"
-	}
 
-	@Test
-	void testHelloWithName() throws Exception {
-		mockMvc.perform(get("/hello").param("name", "Alonso"))
-				.andExpect(status().isOk())
-				.andExpect(content().string("Hello Alonso!"));
-	}
+
 }
